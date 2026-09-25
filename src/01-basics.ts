a29159d7326e523f3d336d14eaf5a00c7bdf9653
Conor <147147702+Conor-20105865@users.noreply.github.com>
2026-09-25 15:34:40 +0100

    Complete type-saftey to colleagues data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -23,7 +23,21 @@ const friends : Friend[] = [friend1, friend2];
 console.log(friends[1]);
 
 //   -------------------
-const colleague1= {
+interface Colleague {
+  name: string;
+  department: string;
+  contact: {
+    email: string;
+    extension: number;
+  };
+}
+
+interface ColleagueHistory {
+  current: Colleague[],
+  former: Colleague[]
+}
+
+const colleague1: Colleague = {
   name: "Ralph Graham",
   department: "Engineering",
   contact: {
@@ -32,7 +46,7 @@ const colleague1= {
   },
 };
 
-const colleague2 = {
+const colleague2: Colleague = {
   name: "Patti Burke",
   department: "Finance",
   contact: {
@@ -41,7 +55,7 @@ const colleague2 = {
   },
 };
 
-const colleague3 = {
+const colleague3: Colleague = {
   name: "Dean Sullivan",
   department: "HR",
   contact: {
@@ -49,7 +63,7 @@ const colleague3 = {
     extension: 125,
   },
 };
-const colleagues = {
+export const colleagues: ColleagueHistory = {
   current: [colleague1, colleague2, colleague3],
   former: [],
 };
